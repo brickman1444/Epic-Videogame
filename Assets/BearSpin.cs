c10febc3b1ef09c5b383411bcf8954989e0df060
Zac Gross <brickman1444@gmail.com>
2015-09-29 23:37:11 -0500

    Adds Bear Spin Script

---
+++ Assets/BearSpin.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearSpin : MonoBehaviour {
+
+    [SerializeField] float spinSpeed = 0.0f;
+    [SerializeField] KeyCode spinKey;
+
+    bool spinning = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ( !spinning && Input.GetKeyDown(spinKey) )
+        {
+            Spin();
+        }
+
+	}
+
+    void Spin()
+    {
+        StartCoroutine(SpinRoutine());
+    }
+
+    IEnumerator SpinRoutine()
+    {
+        spinning = true;
+        float angle = 0;
+        while (angle < 360)
+        {
+            angle += Time.deltaTime * spinSpeed;
+            Debug.Log(angle);
+            transform.Rotate(Vector3.forward, Time.deltaTime * spinSpeed);
+            yield return null;
+        }
+        spinning = false;
+    }
+}
