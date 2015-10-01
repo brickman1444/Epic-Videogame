5493f81b76f903b79700ba5614d32ff592c919da
Zac Gross <brickman1444@gmail.com>
2015-09-30 23:41:52 -0500

    Add curbs and respawn

---
+++ Assets/OffRoadRespawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffRoadRespawn : MonoBehaviour {
+
+    Vector3 respawnPos = Vector3.zero;
+    Rigidbody rigidbody = null;
+
+	// Use this for initialization
+	void Start () {
+        respawnPos = transform.position;
+        rigidbody = GetComponent<Rigidbody>();
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Road")
+        {
+            transform.position = respawnPos;
+            rigidbody.velocity = Vector3.zero;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
