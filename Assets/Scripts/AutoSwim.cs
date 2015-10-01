79f26f8f55b65c3cd5e70582c5dcd914350e1299
Zac Gross <brickman1444@gmail.com>
2015-10-01 12:39:13 -0500

    Make water bigger and add school of fish.

---
+++ Assets/Scripts/AutoSwim.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoSwim : MonoBehaviour {
+
+    [SerializeField] float sideSwimTime = 0.0f;
+
+    float startSwimTime = 0.0f;
+
+    bool isSwimmingLeft = true;
+
+    Swim swim = null;
+
+	// Use this for initialization
+	void Start () {
+        startSwimTime = Time.time;
+        swim = GetComponent<Swim>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time - startSwimTime > sideSwimTime)
+        {
+            isSwimmingLeft = !isSwimmingLeft;
+            startSwimTime = Time.time;
+        }
+
+        if (isSwimmingLeft)
+        {
+            swim.SwimLeft();
+        }
+        else
+        {
+
+            swim.SwimRight();
+        }
+
+	}
+}
