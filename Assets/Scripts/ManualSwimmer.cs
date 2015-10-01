6e1e3f249e11ce8905962760dd8c61db3c3d87b3
Zac Gross <brickman1444@gmail.com>
2015-10-01 12:22:06 -0500

    Add swimming

---
+++ Assets/Scripts/ManualSwimmer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManualSwimmer : MonoBehaviour {
+
+    [SerializeField] KeyCode leftButton;
+    [SerializeField] KeyCode rightButton;
+
+    Swim swim = null;
+
+	// Use this for initialization
+	void Start () {
+        swim = GetComponent<Swim>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(leftButton))
+        {
+            swim.SwimLeft();
+        }
+        else if (Input.GetKey(rightButton) )
+        {
+            swim.SwimRight();
+        }
+
+	}
+}
---
+++ Assets/Scripts/Swim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swim : MonoBehaviour {
+
+    [SerializeField] float forwardSpeed = 0.0f;
+    [SerializeField] float sideSpeed = 0.0f;
+
+    public void SwimLeft()
+    {
+        transform.position += Vector3.right * Time.deltaTime * forwardSpeed + Vector3.up * Time.deltaTime * sideSpeed;
+    }
+
+    public void SwimRight()
+    {
+        transform.position += Vector3.right * Time.deltaTime * forwardSpeed + Vector3.up * Time.deltaTime * -sideSpeed;
+    }
+}
