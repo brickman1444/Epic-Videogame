d40a2a836d981e8c775b25a348113a1bf2ae794b
Zac Gross <brickman1444@gmail.com>
2015-10-11 19:22:16 -0500

    Organization and basic drag and drop callbacks.

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragAndDrop : MonoBehaviour {
+
+    [SerializeField,ReadOnly]
+    bool isBeingDragged = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse down");
+        isBeingDragged = true;
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("Mouse up");
+        isBeingDragged = false;
+    }
+}
