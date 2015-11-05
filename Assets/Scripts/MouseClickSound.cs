2116747a0fc5f27b481c2f1f97c3e83f520beaec
Zac Gross <brickman1444@gmail.com>
2015-11-05 04:50:35 -0600

    Add mouse click sounds. Separate sounds for mouse down and mouse up because click and drag is such a big part

---
+++ Assets/Scripts/MouseClickSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClickSound : MonoBehaviour {
+
+
+    [SerializeField]
+    AudioSource mouseDownAudio;
+    [SerializeField]
+    AudioSource mouseUpAudio;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDownAudio.Play();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseUpAudio.Play();
+        }
+	}
+}
