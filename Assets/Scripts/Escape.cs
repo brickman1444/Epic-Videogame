908f9b1e65c1946169383b3e340ca68b0fee196f
Zac Gross <brickman1444@gmail.com>
2015-11-05 21:58:52 -0600

    Add escape to close the game

---
+++ Assets/Scripts/Escape.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Escape : MonoBehaviour {
+
+    [SerializeField]
+    KeyCode quitKey;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(quitKey))
+        {
+            Application.Quit();
+        }
+        
+	}
+}
