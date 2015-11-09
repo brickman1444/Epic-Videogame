f24f54d221f6d210decedbcf26945572a0bc86d7
Zac Gross <brickman1444@gmail.com>
2015-11-09 16:00:46 -0600

    Really add restart button

---
+++ Assets/Scripts/ReloadOnButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReloadOnButton : MonoBehaviour {
+
+    public void Reload()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
